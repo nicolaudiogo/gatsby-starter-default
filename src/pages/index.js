55e86174229d0a32519b3da4c72b95e74c2fa1e8
DESKTOP-6TTLMHD\joaod <nicolaudiogo@gmail.com>
2018-10-01 19:48:24 -0300

    002

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+	<h2> Vai todo mundo pro inferno</h2>
   </Layout>
 )
 
